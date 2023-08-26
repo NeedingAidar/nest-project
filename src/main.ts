d35510cba5c80fe2d031ac050f2357bb1c21ed5e
Jaringyad <a25142483@gmail.com>
2023-08-26 12:41:48 +0300

    added filters and .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WrapResponseInterceptor } from './filters/wrap-response.interceptor';
+import { ValidationPipe } from './filters/validation.pipe';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.useGlobalInterceptors(new WrapResponseInterceptor())
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
 }
 bootstrap();
