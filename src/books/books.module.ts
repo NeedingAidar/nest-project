9c5280cbfbeec17bb2f24b826f97780c87afc2aa
Jaringyad <a25142483@gmail.com>
2023-08-27 13:17:08 +0300

    added imports

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,10 +1,15 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './model/book.model';
 import { Module } from '@nestjs/common';
+import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
   ],
+  controllers: [BooksController],
+  providers: [BooksService]
+  
 })
 export class BooksModule {}
