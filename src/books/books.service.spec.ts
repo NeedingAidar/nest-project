79e8e33e0232cb89d62b49b0247f3e00b5e19c24
Jaringyad <a25142483@gmail.com>
2023-08-29 04:05:26 +0300

    added first test to books service

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('BooksService', () => {
   let service: BooksService;
+  let mockModel;
 
   beforeEach(async () => {
+    mockModel = {
+      find: jest.fn().mockReturnValue({
+        exec: jest.fn().mockResolvedValue([]),
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [
+        BooksService,
+        {
+          provide: getModelToken('Book'),
+          useValue: mockModel,
+        },
+      ],
     }).compile();
 
     service = module.get<BooksService>(BooksService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return an array of books', async () => {
+    expect(await service.findAll()).toEqual([]);
+    expect(mockModel.find).toHaveBeenCalled();
   });
+
+  // Добавьте другие тесты для остальных методов в BooksService
 });
